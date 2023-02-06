f673cf4ace641621fe801bde315dd8f1aa7f7c25
guilhermesmatiola <guimatiola@gmail.com>
2023-02-06 13:59:05 -0300

    auth/connection to route test nfc api

--- src/components/services/urls.js
+++ src/components/services/urls.js
@@ -1,17 +1,17 @@
-// const url = {
-// 	signup: "https://nfc-zpl-teste.onrender.com/signup",
-// 	signin: "https://nfc-zpl-teste.onrender.com/signin",
-// 	teste: "https://nfc-zpl-teste.onrender.com/teste",
-// 	nfc: "https://nfc-zpl-teste.onrender.com/nfc",
-// 	nfc: "https://nfc-zpl-teste.onrender.com/nfc",
-// };
-
 const url = {
-	signup: "http://localhost:5000/signup",
-	signin: "http://localhost:5000/signin",
-	teste: "http://localhost:5000/teste",
-	nfc: "http://localhost:5000/nfc",
-	nfc: "http://localhost:5000/nfc",
+	signup: "https://nfc-zpl-teste.onrender.com/signup",
+	signin: "https://nfc-zpl-teste.onrender.com/signin",
+	teste: "https://nfc-zpl-teste.onrender.com/teste",
+	nfc: "https://nfc-zpl-teste.onrender.com/nfc",
+	nfc: "https://nfc-zpl-teste.onrender.com/nfc",
 };
+
+// const url = {
+// 	signup: "http://localhost:5000/signup",
+// 	signin: "http://localhost:5000/signin",
+// 	teste: "http://localhost:5000/teste",
+// 	nfc: "http://localhost:5000/nfc",
+// 	nfc: "http://localhost:5000/nfc",
+// };
 
 export default url;
